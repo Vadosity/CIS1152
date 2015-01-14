31793ccdfe749278d0be2e0c9f7977199cab170d
unknown <kpw11181@CLK310-08.vtc.vsc.edu>
2015-01-13 20:02:58 -0500

    Project

--- Assignments/Lab_1/index.php
+++ Assignments/Lab_1/index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
 <title> Hello World </title>
-<link rel="stylesheet" href="./css/styles.css" type="css/text" />
+<link rel="stylesheet" href=".\css\doesitwork.css" type="text/css" />
+<meta charset="utf-8">
 </head>
 <body>
 <p> Hello world! </p>
