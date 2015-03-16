012e782d311d665880e2ba82baf3b9e2daee6a8e
unknown <kpw11181@CON104-11.vtc.vsc.edu>
2015-03-16 15:36:03 -0400

    lab 6

--- Assignments/Lab_6/index.php
+++ Assignments/Lab_6/index.php
@@ -1,10 +1,35 @@
 <?php
 
 require_once "src/PageLayout.php";
+require_once "src/math_fun.php";
 
 use VTC\Lab_5\MathFun as MathFun;
 use VTC\Lab_5\PageLayout as PageLayout;
 
+
+$math = new MathFun\MathFun;
+
+if (isset($_POST['submit'])) {
+    $truncateFloatResult = $math -> truncateFloat($_POST["truncateFloat"]);
+    $farenheit2KelvinResult = $math -> fahrenheit2Kelvin($_POST["fahrenheit2Kelvin"]);
+    $dodecahedronVolumeResult = $math -> dodecahedronVolume($_POST["dodecahedronVolume"]);
+    $impactVelocityResult = $math -> impactVelocity($_POST["impactVelocity"]);
+} else {
+    $truncateFloat = "";
+    $fahrenheit2Kelvin = "";
+    $dodecahedronVolume = "";
+    $impactVelocity = "";
+}
+
 echo PageLayout\PageLayout::$header;
-echo PageLayout\PageLayout::$form_layout;
+
+if (!isset($_POST['submit'])) {
+
+    echo PageLayout\PageLayout::$form_results;
+} else{
+	PageLayout\PageLayout::formResults($truncateFloatResult, $fahrenheit2KelvinResult, $dodecahedronVolumeResult, $impactVelocityResult);
+	echo PageLayout\PageLayout::$form_results;
+}
+
 echo PageLayout\PageLayout::$footer;
+?>
--- Assignments/Lab_6/src/PageLayout.php
+++ Assignments/Lab_6/src/PageLayout.php
@@ -23,6 +23,24 @@ namespace VTC\Lab_5\PageLayout;
 
 class PageLayout
 {
+	public static function formResults($truncateFloat, $fahrenheit2Kelvin, $dodecahedronVolume, $impactVelocity){
+		
+		$form_results = "";
+    if (!empty($truncateFloatResult)) {
+        $form_results .= "The truncated floating point value is: " . $truncateFloatResult . ".<br>";
+    }
+    if (!empty($fahrenheit2KelvinResult)) {
+        $form_results .= "The Kelvin value is: " . $fahrenheit2KelvinResult . ".<br>";
+    }
+    if (!empty($dodecahedronVolumeResult)) {
+        $form_results .= "The dodecahedron volume value is: " . $dodecahedronVolumeResult . ".<br>";
+    }
+    if (!empty($impactVelocityResult)) {
+        $form_results .= "The splat value is: " . $impactVelocityResult . ".<br>";
+    }
+	
+	echo $form_results;
+	}
 
     public static $header = "<html>\n\t<head>\n\t\t<title>Simple PHP Form Example</title>\n\t</head>\n\t<body>";
     public static $footer = "\n\t</body>\n</html>";
@@ -30,7 +48,7 @@ class PageLayout
 <p>
 <form method="post" action="">
     <label name="truncateFloat">Floating Point Value</label><input type="text" name="truncateFloat"><br>
-    <label name="farenheit2Kelvin">Farenheit Value</label><input type="text" name="farenheit2Kelvin"><br>
+    <label name="farenheit2Kelvin">Fahrenheit Value</label><input type="text" name="farenheit2Kelvin"><br>
     <label name="dodecahedronVolume">Dodecahedron Side Value</label><input type="text" name="dodecahedronVolume"><br>
     <label name="impactVelocity">Height of Fall Value</label><input type="text" name="impactVelocity"><br>
     <input type="submit" value="submit" name="submit">
